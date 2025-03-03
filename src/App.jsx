62dd26578bfd1e8fade984a1cf940150561c9659
dariuscruceru21 <dariuscruceru@gmail.com>
2025-03-03 22:19:06 +0200

    implemented search funcitonalitie and optimized it by debouncing the input field

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
+import {useDebounce} from 'react-use';
 import Search from "./components/Search.jsx";
 import Spinner from "./components/Spinner.jsx";
 import MovieCard from "./components/MovieCard.jsx";
@@ -19,6 +20,9 @@ const App = () => {
     const [errorMessage, setErrorMessage] = useState('');
     const [movieList, setMovieList] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
+
+    useDebounce(()=> setDebouncedSearchTerm(searchTerm), 500, [searchTerm]);
 
     const fetchMovies = async (query = '') => {
         setIsLoading(true);
@@ -31,13 +35,13 @@ const App = () => {
 
             const response = await fetch(endpoint, API_OPTIONS);
 
-            if(!response.ok) {
+            if (!response.ok) {
                 throw new Error('Failed to fetch movies');
             }
 
             const data = await response.json();
 
-            if(data.Response === 'False') {
+            if (data.Response === 'False') {
                 setErrorMessage(data.Error || 'Failed to fetch movies');
                 setMovieList([]);
                 return;
@@ -54,8 +58,8 @@ const App = () => {
     };
 
     useEffect(() => {
-        fetchMovies();
-    }, []);
+        fetchMovies(debouncedSearchTerm);
+    }, [debouncedSearchTerm]);
 
     return (
         <main>
@@ -63,10 +67,10 @@ const App = () => {
 
             <div className="wrapper">
                 <header>
-                    <img src="./hero.png" alt="Hero Banner" />
+                    <img src="/hero.png" alt="Hero Banner"/>
                     <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
 
-                    <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+                    <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
                 </header>
 
 
@@ -74,13 +78,13 @@ const App = () => {
                     <h2>All Movies</h2>
 
                     {isLoading ? (
-                        <Spinner />
+                        <Spinner/>
                     ) : errorMessage ? (
                         <p className="text-red-500">{errorMessage}</p>
                     ) : (
                         <ul>
                             {movieList.map((movie) => (
-                                <MovieCard key={movie.id} movie={movie} />
+                                <MovieCard key={movie.id} movie={movie}/>
                             ))}
                         </ul>
                     )}
